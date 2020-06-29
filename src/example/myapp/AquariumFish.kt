b37c8e7514383a7ebaabdab583e9d765ef1ad800
Rony Benny <ronybenny2812@gmail.com>
2020-06-29 16:04:47 +0530

    updates

--- src/example/myapp/AquariumFish.kt
+++ src/example/myapp/AquariumFish.kt
@@ -1,5 +1,6 @@
 package example.myapp
 
+/*
 abstract class AquariumFish{
     abstract val color:String
 }
@@ -20,4 +21,45 @@ class Plecostomus: AquariumFish(),FishAction {
 
 interface FishAction{
     fun eat()
+}
+*/
+
+
+//INTERFACE DELEGATION
+//          ||
+//         \  /
+//          \/
+
+class Shark: FishAction,FishColor {
+    override val color = "gray"
+    override fun eat(){
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus(fishColor: FishColor = GoldColor): FishAction by PrintingFishAction("eat algae")
+        ,FishColor by fishColor{
+//    override val color = "gold"
+
+//    override fun eat(){
+//        println("eat algae")
+//    }
+}
+
+interface FishAction{
+    fun eat()
+}
+
+interface FishColor {
+    val color: String
+}
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
 }
