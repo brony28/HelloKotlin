81a4fbb46206ba7b311f688f7ad0601df1e2ab16
Rony Benny <ronybenny2812@gmail.com>
2020-07-02 18:22:42 +0530

    Higher Order functions and some extended functions

---
+++ src/examples/Fish.kt
@@ -0,0 +1,44 @@
+package examples
+
+data class Fish(var name:String)
+
+fun fishExamples(){
+    val fish = Fish("splashy")
+    with(fish.name) {
+        this.capitalize()
+        println(this.capitalize()) // the this is implicit and not needed, so you can also remove it.
+    }
+
+    fish.run { println(name) }
+
+    val fish2 = Fish("clash").apply { name = "royale" }
+    println(fish2.name)
+
+    //run() and apply() are similar, but run() returns the result of applying the function, and apply() returns the object after applying the function.
+
+    println(fish.let { it.name.capitalize() }.let { it+"fish" }.let { it.length }.let { it+31 })
+    //In this example, the object type referred to by it is Fish, then String, then String again and finally Int.
+
+    println(fish)
+
+}
+//with() is a higher-order function
+
+
+fun myWith(name:String,block: String.() -> Unit){
+    name.block()
+}
+
+fun fishExamples2(){
+    val fish = Fish("rony benny")
+    myWith(fish.name) {
+        this.capitalize()
+        println(this.capitalize()) // the this is implicit and not needed, so you can also remove it.
+    }
+}
+
+
+fun main(){
+    fishExamples()
+    fishExamples2()
+}
